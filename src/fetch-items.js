fb5cfa28dcacf50aec2e43c00fad9cfec1dfa82c
wasd <wasdsweden@gmail.com>
2017-03-23 21:23:46 +0100

    Use each() instead of all()

--- src/fetch-items.js
+++ src/fetch-items.js
@@ -31,38 +31,38 @@ db.all("SELECT * FROM items", function(err, rows) {
 
 //db.run("BEGIN TRANSACTION");
 db.serialize(function() {
-  db.all("SELECT id, url, title FROM categories", function(err, rows) {
-    rows.forEach(function(row) {
-      const categoryId = row.id;
-      if (categoryId != 6) {
-        return true; // Be nice when developing, only process one url bellow
+  db.each("SELECT id, url, title FROM categories", function(err, row) {
+    const categoryId = row.id;
+    if (categoryId != 6) {
+      return true; // Be nice when developing, only process one url bellow
+    }
+    const url = baseURL + row.url;
+    request(url, function(error, response, html) {
+      if (error) {
+        console.log(error);
+        return;
       }
-      const url = baseURL + row.url;
-      request(url, function(error, response, html) {
-        if (error) {
-          console.log(error);
-          return;
-        }
-        const $ = cheerio.load(html);
-        const products = $("div.prd > a");
-        //FIXME items can have many categories
-        db.run("BEGIN TRANSACTION");
-        let stmt = db.prepare("INSERT INTO items (category_id, url, img_url, name, price, discount, first_seen, last_seen) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-        //products.each(function(id, element) {
-        for (let i = 0; i < products.length; i++) {
-          const element = products[i];
-          const itemURL = element.attribs.href;
-          const itemImageURL = "http:" + $(element).find("img.zoom").first()[0].attribs.src.replace(/\?itok=.*/g, '');
-          const name = $(element).find("span.prd-name").first().text().replace(/^\s+|\s+$/g, '');
-          const price = $(element).find("div.prd-price-num").first().text().match(/\d+/)[0]; //FIXME doesn't understand that it's a sum price
-          const discount = $(element).find("span.prd-discount-oldprice > span").first().text().replace(/^[^\(]+..|..[^\)]+$/g, '');
-          console.log("..." + price);
-          stmt.run(categoryId, itemURL, itemImageURL, name, price, discount, TIMESTAMP_NOW, TIMESTAMP_NOW);
-        }
-        //});
-        db.run("COMMIT");
-      });
-    })
+      const $ = cheerio.load(html);
+      const products = $("div.prd > a");
+      //FIXME items can have many categories
+      db.run("BEGIN TRANSACTION");
+      let stmt = db.prepare("INSERT INTO items (category_id, url, img_url, name, price, discount, first_seen, last_seen) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+      //products.each(function(id, element) {
+      for (let i = 0; i < products.length; i++) {
+        const element = products[i];
+        const itemURL = element.attribs.href;
+        const itemImageURL = "http:" + $(element).find("img.zoom").first()[0].attribs.src.replace(/\?itok=.*/g, '');
+        const name = $(element).find("span.prd-name").first().text().replace(/^\s+|\s+$/g, '');
+        const price = $(element).find("div.prd-price-num").first().text().match(/\d+/)[0]; //FIXME doesn't understand that it's a sum price
+        const discount = $(element).find("span.prd-discount-oldprice > span").first().text().replace(/^[^\(]+..|..[^\)]+$/g, '');
+        //console.log("..." + price);
+        stmt.run(categoryId, itemURL, itemImageURL, name, price, discount, TIMESTAMP_NOW, TIMESTAMP_NOW);
+      }
+      //});
+      db.run("COMMIT");
+    });
+
   });
 });
+
 // db.close();
