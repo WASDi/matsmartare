d45a745e9fc529720c8d2468c2865b3eb346a88b
wasd <wasdsweden@gmail.com>
2017-04-01 11:55:21 +0200

    Begin work on diffing categories

---
+++ server/test/categoriesDiff.js
@@ -0,0 +1,19 @@
+import request from 'request';
+import cheerio from 'cheerio';
+import sqlite3 from 'sqlite3';
+
+import { newCategory } from "../src/models.js";
+
+const db = new sqlite3.Database("matsmartare.db");
+db.all("SELECT id, url, title FROM categories", function(err, rows) {
+  const dbCategories = rows.map(x => newCategory(x.id, x.url, x.title));
+  console.log(dbCategories);
+
+  // const extURL = "http://www.matsmart.se/";
+  //
+  // request(extURL, function(error, response, html) {
+  //
+  // });
+
+
+});
