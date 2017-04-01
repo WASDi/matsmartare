2eec0ab99627920e9a9f436dbac25de5d6545c96
wasd <wasdsweden@gmail.com>
2017-04-01 14:31:06 +0200

    Added options to dropdowns

---
+++ client/src/components/Dropdown.js
@@ -0,0 +1,33 @@
+import React, { PureComponent } from 'react';
+import Select from 'react-select';
+
+export default class Dropdown extends PureComponent {
+
+  constructor (props) {
+    super(props);
+    this.state = {};
+    this.updateValue = this.updateValue.bind(this);
+  }
+
+  updateValue (newValue) {
+		this.setState({
+			selectValue: newValue
+		});
+	}
+
+  render () {
+    const {placeholder, align, options} = this.props;
+    return (
+      <div className="dropdown" style={{float: align}}>
+        <Select
+          name="form-field-name"
+          value={this.state.selectValue}
+          onChange={this.updateValue}
+          options={options}
+          searchable={false}
+          placeholder={placeholder}
+        />
+      </div>
+    )
+  }
+}
--- client/src/components/SelectPanel.js
+++ client/src/components/SelectPanel.js
@@ -1,47 +1,35 @@
 import React, { PureComponent } from 'react';
-import Select from 'react-select';
-import 'react-select/dist/react-select.css';
+import Dropdown from './Dropdown.js';
 
+import 'react-select/dist/react-select.css';
 import './SelectPanel.css';
 
-function logChange(val) {
-    console.log("Selected: " + val.value);
-}
+const filterOptions = [
+    { value: 'xxx', label: 'TODO' }
+];
 
-let options = [
-    { value: 'one', label: 'One' },
-    { value: 'two', label: 'Two' }
+const sortOptions = [
+    { value: 'xxx', label: 'Billigast' },
+    { value: 'xxx', label: 'Mest rabatt' },
+
+    { value: 'xxx', label: 'Senast inkomna' },
+    { value: 'xxx', label: 'Bäst före' },
+
+    { value: 'xxx', label: 'Dyrast' },
+    { value: 'xxx', label: 'Minst rabatt' }
 ];
 
 export default class SelectPanel extends PureComponent {
 
-  constructor (props) {
-    super(props);
-  }
+  // constructor (props) {
+  //   super(props);
+  // }
 
   render () {
     return (
       <div>
-        <div className="dropdown" style={{float: "left"}}>
-          <Select
-            name="form-field-name"
-            value=""
-            options={options}
-            onChange={logChange}
-            searchable={false}
-            placeholder="Filtrera..."
-          />
-        </div>
-        <div className="dropdown" style={{float: "right"}}>
-          <Select
-            name="form-field-name"
-            value=""
-            options={options}
-            onChange={logChange}
-            searchable={false}
-            placeholder="Sortera..."
-          />
-        </div>
+        <Dropdown options={filterOptions} placeholder="Filtrera..." align="left" />
+        <Dropdown options={sortOptions} placeholder="Sortera..." align="right" />
       </div>
     )
   }
