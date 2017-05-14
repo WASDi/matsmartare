a53fd2dab0064c20e8d8a3d8bdc71c51094ee207
wasd <wasdsweden@gmail.com>
2017-05-14 13:38:20 +0200

    Update categories in database when diff found

--- server/test/categoriesDiff.js
+++ server/test/categoriesDiff.js
@@ -7,12 +7,16 @@ import {
 } from "../src/lib/models.js";
 import fetchCategories from '../src/lib/fetch-categories.js';
 
-function findDiff(dbCategories, webCategories) {
+function findAndRepairDiff(dbCategories, webCategories, db) {
   let diffFound = false;
   if (dbCategories.length !== webCategories.length) {
     console.log(" ##### DIFFERENT LENGTH ##### DB = " + dbCategories.length + ", WEB = " + webCategories.length);
     diffFound = true;
   }
+
+  db.run("BEGIN TRANSACTION");
+  let insertStmt = db.prepare("INSERT INTO categories (url, name) VALUES (?, ?)");
+  let deleteStmt = db.prepare("DELETE FROM categories WHERE id = ?");
 
   // check everything in DB, if it also exists in WEB
   for (let db_idx = 0; db_idx < dbCategories.length; db_idx++) {
@@ -32,6 +36,7 @@ function findDiff(dbCategories, webCategories) {
     if (!webAlsoHasUrl || !webAlsoHasName) {
       console.log(" ##### DIFF: DB has " + dbCat.name + " with url " + dbCat.url + " but WEB does not");
       diffFound = true;
+      deleteStmt.run(dbCat.id);
     }
   }
 
@@ -53,11 +58,16 @@ function findDiff(dbCategories, webCategories) {
     if (!dbAlsoHasUrl || !dbAlsoHasName) {
       console.log(" ##### DIFF: WEB has " + webCat.name + " with url " + webCat.url + " but DB does not");
       diffFound = true;
+      insertStmt.run(webCat.url, webCat.name);
     }
   }
 
+  insertStmt.finalize();
+  deleteStmt.finalize();
+  db.run("COMMIT");
+
   if (!diffFound) {
-    console.log(" ----- no diffs -----");
+    console.log(" ----- no category diffs -----");
   }
 }
 
@@ -70,7 +80,7 @@ db.all("SELECT id, url, name FROM categories", function(err, rows) {
   const dbCategories = rows.map(x => newCategory(x.id, x.url, x.name));
 
   fetchCategories(function(webCategories) {
-    findDiff(dbCategories, webCategories);
+    findAndRepairDiff(dbCategories, webCategories, db);
     db.close();
   });
 });
