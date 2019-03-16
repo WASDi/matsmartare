50a8905b635b47c69bef13c1656493f1774bb36f
wasd <wasdsweden@gmail.com>
2019-03-16 23:02:10 +0100

    fixed update_avg_discounts.sh

--- update_avg_discounts.sh
+++ update_avg_discounts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sqlite3 matsmartare.db "INSERT INTO avg_discounts (the_date, avg_discount) SELECT current_date, avg(discount) FROM items WHERE name NOT LIKE 'ARCHIVE_%'"
+sqlite3 matsmartare.db "INSERT INTO avg_discounts (the_date, avg_discount) SELECT current_date, avg(discount) FROM items WHERE last_seen=(SELECT MAX(last_seen) FROM items)"
