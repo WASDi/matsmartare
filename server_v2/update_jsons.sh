55898a147089eedcc1a33e7fb92c41332c391c69
wasd <wasdsweden@gmail.com>
2019-05-02 20:39:59 +0200

    Collapse folder

--- server_v2/update_jsons.sh
+++ server_v2/update_jsons.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd jsons/latest
+cd git_ignore/jsons/latest
 curl 'https://api.matsmart.se/api/v1.0/routes?market=SE' > routes.json
 curl 'https://api.matsmart.se/api/v1.0/product-displays?market=SE' > products.json
 python3 -m json.tool < routes.json > routes_formatted.json
